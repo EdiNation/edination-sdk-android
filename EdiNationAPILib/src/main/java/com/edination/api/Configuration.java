748253fde00fb45c2e035570613894cf13b4c786
Kamen Nikolov <kamen.nikolov@edination.com>
2020-02-14 18:33:15 +0000

    Initial commit

---
+++ EdiNationAPILib/src/main/java/com/edination/api/Configuration.java
@@ -0,0 +1,17 @@
+/*
+ * EdiNationAPILib
+ *
+ * This file was automatically generated for EdiNation by APIMATIC v2.0 ( https://apimatic.io ).
+ */
+package com.edination.api;
+
+
+public class Configuration {
+    //The base Uri for API calls
+    public static String baseUri = "https://api.edination.com/v2";
+
+    //API key to authenticate requests
+    //TODO: Replace the ocpApimSubscriptionKey with an appropriate value
+    public static String ocpApimSubscriptionKey = "";
+
+}
---
+++ EdiNationAPILib/src/main/java/com/edination/api/models/UNE.java
@@ -0,0 +1,55 @@
+/*
+ * EdiNationAPILib
+ *
+ * This file was automatically generated for EdiNation by APIMATIC v2.0 ( https://apimatic.io ).
+ */
+package com.edination.api.models;
+
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.ALWAYS)
+public class UNE 
+        extends java.util.Observable
+        implements java.io.Serializable {
+    private static final long serialVersionUID = -3859131239232600031L;
+    private String groupControlCount1;
+    private String groupReferenceNumber2;
+    /** GETTER
+     * TODO: Write general description for this method
+     */
+    @JsonGetter("GroupControlCount_1")
+    public String getGroupControlCount1 ( ) { 
+        return this.groupControlCount1;
+    }
+    
+    /** SETTER
+     * TODO: Write general description for this method
+     */
+    @JsonSetter("GroupControlCount_1")
+    public void setGroupControlCount1 (String value) { 
+        this.groupControlCount1 = value;
+        notifyObservers(this.groupControlCount1);
+    }
+ 
+    /** GETTER
+     * TODO: Write general description for this method
+     */
+    @JsonGetter("GroupReferenceNumber_2")
+    public String getGroupReferenceNumber2 ( ) { 
+        return this.groupReferenceNumber2;
+    }
+    
+    /** SETTER
+     * TODO: Write general description for this method
+     */
+    @JsonSetter("GroupReferenceNumber_2")
+    public void setGroupReferenceNumber2 (String value) { 
+        this.groupReferenceNumber2 = value;
+        notifyObservers(this.groupReferenceNumber2);
+    }
+ 
+}
